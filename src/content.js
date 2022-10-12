6bb2ec47171caad7ae2409c9bf461fd597dca928
SHIVAM20S <78133558+SHIVAM20S@users.noreply.github.com>
2022-10-12 14:43:43 +0530

    Add files via upload

---
+++ src/content.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div align="center">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-10" align="left">
+        <img src="https://www.wildfutures.org/assets/img/adopt_header.jpg" height="100%"></img><br></br>
+</div>
+
+
+
+        </div>
+
+        <div class="row">
+
+
+        {/* <div class="" style="margin-right: 20px">
+                <p><strong>You Will Receive:</strong></p>
+
+                <ul>
+                    <li>A cuddly monkey toy (optional)</li>
+                    <li>Personalised certificate of your adoption</li>
+                    <li>Photo of your adopted monkey</li>
+                    <li>Your monkey’s story</li>
+                    <li>Species factsheet</li>
+                    <li>‘Wild Futures’ bi-annual newsletters</li>
+                    <li>Discount to our Monkey Sanctuary</li>
+                </ul>
+                <br />
+            </div> */}
+            <div class="col-md-2"></div>
+<div class="col-md-5 bx" ><p><strong>You Will Receive:</strong></p>
+
+<ul>
+    <li>A cuddly monkey toy (optional)</li>
+    <li>Personalised certificate of your adoption</li>
+    <li>Photo of your adopted monkey</li>
+    <li>Your monkey’s story</li>
+    <li>Species factsheet</li>
+    <li>‘Wild Futures’ bi-annual newsletters</li>
+    <li>Discount to our Monkey Sanctuary</li>
+</ul>
+<br /></div>
+
+<div class="col-md-5">
+            <p class="lead"><strong>Adopt a monkey and protect primates worldwide.</strong></p>
+            <a id="adopt-now" href="#monkeys" class="btn btn-primary btn-lg">Adopt Now</a>
+            <br /><br />
+            <p>Deforestation and the bush meat trade threaten primate species worldwide, leading to forty three species now considered to be Critically Endangered. The primate pet trade is also a huge problem causing suffering and neglect, with about 5,000 privately owned primates in the UK alone.</p>
+            <p>Help our charity, Wild Futures, protect primates in the UK and abroad by adopting a monkey today.</p>
+            <br />
+
+</div>
+
+
+            
+        </div>
+
+
+<div class="row sa">
+
+<div class="col-md-2 pd"><a><img src="https://www.wildfutures.org/assets/img/adopt_header.jpg" height="100vh"></img></a></div>
+<div class="col-md-2 pd"><img src="https://www.wildfutures.org/assets/img/adopt_header.jpg" height="100vh"></img></div>
+<div class="col-md-2 pd"><img src="https://www.wildfutures.org/assets/img/adopt_header.jpg" height="100vh"></img></div>
+<div class="col-md-2 pd"><img src="https://www.wildfutures.org/assets/img/adopt_header.jpg" height="100vh"></img></div>
+
+
+
+
+</div>
+
+
+
+
+    </div>
+  )
+}
---
+++ src/header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"><h3>MonkeyBay</h3></a>
+    <div class="collapse navbar-collapse al" id="navbarTogglerDemo03">
+      <ul class="navbar-nav " >
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+    </div>
+  )
+}
